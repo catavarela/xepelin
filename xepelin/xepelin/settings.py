882cb3a2b914a75cbd07bd3841d04f50cefe0096
catavarela <cavarela@itba.edu.ar>
2022-07-31 22:17:57 -0300

    Update settings.py

--- xepelin/xepelin/settings.py
+++ xepelin/xepelin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s_ayc#$zr2181+o-^uzjnl1@+ky*(+=q223$h(nr+9d@wt(a4!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.229.133.127']
 
 
 # Application definition
